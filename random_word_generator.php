57a983d96350d70c134893dd8b40e0c0c24e51f7
CJ Stritzel <cj.stritzel@popart.com>
2014-10-10 12:54:27 -0700

    changing regex pattern to accomodate 'new' CL structure

--- random_word_generator.php
+++ random_word_generator.php
@@ -68,15 +68,15 @@
 <body>
 <?php
   class random_word {
-    public function __construct($how_many_words = 1, $print = 0) {
+    public function __construct($how_many_words = 1, $debug = 0) {
       $how_many_words = ($how_many_words > 50 || $how_many_words < 0 || !intval($how_many_words)) ? 50 : intval($how_many_words) ;
-      $boring         = "free next postings nbsp craigslist more with what that there them must their this also were some your have from when they these does ikea";
+      $boring         = "free next postings nbsp misc craigslist more with what that there them must their this also were some your have from when they these does ikea";
       $url            = "http://portland.craigslist.org/zip/";
-      $pattern        = "/<p.*?>.*?<a.*?>(.*?)<\/a>.*?<\/p>/";
+      $pattern        = "/<p class=\"row\" data-pid=.*?>.*?<a.*?class=\"hdrlnk\">(.*?)<\/a>.*?<\/p>/";
 
       $str            = ereg_replace("[\r|\n]"," ",file_get_contents($url)); // <- Turn page into one string...
       $lower_str      = strtolower($str);                                    // <- Lowercase string...
-      $links_in_paras = preg_match_all($pattern,$lower_str,$m);              // <- Get links inside paragraphs from that string...
+      $links_in_paras = preg_match_all($pattern,$lower_str,$m);              // <- Get links inside paragraphs from that string...    
       $all_alpha      = ereg_replace("[^a-z ]"," ",implode(' ',$m[1]));      // <- Turn that array back to a string and remove all non-alpha chars...
       $not_boring     = str_replace(explode(' ',$boring),' ',$all_alpha);    // <- Remove words from the list of "boring" words...
       $less_than_num  = preg_replace('/\b\w{1,3}\b/',' ',$not_boring);       // <- Remove words of less than 4 chars...
@@ -85,10 +85,10 @@
 
       $tmp['tmp']     = 1; // <- there for count() in while loop...
 
-      while(count($tmp) < ($how_many_words + 1)) {
-        $n                = rand(0,count($text_array) - 1);
+      while(count($tmp) < ($how_many_words + 1)):
+        $n                    = rand(0,count($text_array) - 1);
         $tmp[$text_array[$n]] = 1;
-      }
+      endwhile;
 
       unset($tmp['tmp']);
       $this->words          = array_keys($tmp);
@@ -99,24 +99,24 @@
       // $print = 1 to see the array
       // $print > 1 to see debugging info.
       // ---------------------------------
-      if ($print > 1) {
+      if ($debug > 1):
         $this->debug->message        = "Random Word Generator has a ceiling of 50 words.";
         $this->debug->url            = htmlentities($url);
         $this->debug->view_pattern   = htmlentities($pattern);
         $this->debug->how_many_words = "<b>" . count($text_array) . "</b> words from <b><a href='" . $url . "' target='_blank'>" . $url . "</a></b>.";
         $this->debug->text_array     = $text_array;
-      }
-      if ($print != 0) {
+      endif;
+      if ($debug != 0):
         echo "<code><pre>";
         print_r($this);
         echo "</pre></code>";
-      }
+      endif;
     }
   }
         
 $how_many_words = ($_GET['how_many_words']) ? intval($_GET['how_many_words']) : 10;
-$print          = ($_GET['print']) ? intval($_GET['print']) : 0;
-$w              = new random_word($how_many_words, $print);
+$debug          = ($_GET['debug']) ? intval($_GET['debug']) : 0;
+$w              = new random_word($how_many_words, $debug);
 ?>
 
 <h1>Random Word Generator</h1>
@@ -126,9 +126,9 @@ $w              = new random_word($how_many_words, $print);
   |
   <a href='<?php echo htmlentities($_SERVER['PHP_SELF']); ?>?how_many_words=25'>New Set (25)</a>
   |
-  <a href='<?php echo htmlentities($_SERVER['PHP_SELF']); ?>?print=1'>New Set (some debugging info)</a>
+  <a href='<?php echo htmlentities($_SERVER['PHP_SELF']); ?>?debug=1'>New Set (some debugging info)</a>
   |
-  <a href='<?php echo htmlentities($_SERVER['PHP_SELF']); ?>?print=2'>New Set (more debugging info)</a>
+  <a href='<?php echo htmlentities($_SERVER['PHP_SELF']); ?>?debug=2'>New Set (more debugging info)</a>
 </p>
 
 <p>Single word is "<b><?php echo $w->word ?></b>"
